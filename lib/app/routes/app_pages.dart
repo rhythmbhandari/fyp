a1ad96322813cb01e2e5f73d8337d79b31c13cda
Rhythm Bhandari <69192521+rhythmbhandari@users.noreply.github.com>
2021-12-18 22:48:00 +0545

    Initial pages created using get_cli

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,19 @@
 import 'package:get/get.dart';
 
+import 'package:quicki/app/modules/auth/bindings/auth_binding.dart';
+import 'package:quicki/app/modules/auth/views/auth_view.dart';
+import 'package:quicki/app/modules/history/bindings/history_binding.dart';
+import 'package:quicki/app/modules/history/views/history_view.dart';
 import 'package:quicki/app/modules/home/bindings/home_binding.dart';
 import 'package:quicki/app/modules/home/views/home_view.dart';
+import 'package:quicki/app/modules/notification/bindings/notification_binding.dart';
+import 'package:quicki/app/modules/notification/views/notification_view.dart';
+import 'package:quicki/app/modules/onboarding/bindings/onboarding_binding.dart';
+import 'package:quicki/app/modules/onboarding/views/onboarding_view.dart';
+import 'package:quicki/app/modules/profile/bindings/profile_binding.dart';
+import 'package:quicki/app/modules/profile/views/profile_view.dart';
+import 'package:quicki/app/modules/splash/bindings/splash_binding.dart';
+import 'package:quicki/app/modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +27,36 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.ONBOARDING,
+      page: () => OnboardingView(),
+      binding: OnboardingBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => AuthView(),
+      binding: AuthBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.NOTIFICATION,
+      page: () => NotificationView(),
+      binding: NotificationBinding(),
+    ),
+    GetPage(
+      name: _Paths.HISTORY,
+      page: () => HistoryView(),
+      binding: HistoryBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,8 +5,20 @@ abstract class Routes {
   Routes._();
 
   static const HOME = _Paths.HOME;
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const AUTH = _Paths.AUTH;
+  static const SPLASH = _Paths.SPLASH;
+  static const PROFILE = _Paths.PROFILE;
+  static const NOTIFICATION = _Paths.NOTIFICATION;
+  static const HISTORY = _Paths.HISTORY;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const ONBOARDING = '/onboarding';
+  static const AUTH = '/auth';
+  static const SPLASH = '/splash';
+  static const PROFILE = '/profile';
+  static const NOTIFICATION = '/notification';
+  static const HISTORY = '/history';
 }
